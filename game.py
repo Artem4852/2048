5fd0fa8e5a97d0df29a837141dc7dd053790c8bb
Artem Kovalevskyi <artem4852@MacBook-Pro--Artem.local>
2024-06-21 10:57:52 +0200

    game improvements and bug fixes

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pygame, random, time
 
 screen_size = 600
 board_size = 400
-cell_size = 100
+tile_size = 100
 
 bg = pygame.image.load("bg.png")
 
@@ -11,24 +11,25 @@ screen = pygame.display.set_mode((screen_size, screen_size))
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 36)
 
-board = [[None for _ in range(4)] for _ in range(4)]
+board = [[0 for _ in range(4)] for _ in range(4)]
 
 colors = {
-    2: (255, 161, 125),
-    4: (255, 205, 125),
-    8: (255, 184, 125),
-    16: (255, 138, 125),
-    32: (255, 220, 125),
-    64: (255, 200, 125),
-    128: (176, 100, 70),
-    256: (191, 145, 71),
-    512: (128, 53, 45),
-    1024: (217, 178, 72),
-    2048: (255, 198, 125),
-    4096: (255, 156, 125),
+    2: (255, 161, 125, 255),
+    4: (255, 205, 125, 255),
+    8: (255, 184, 125, 255),
+    16: (255, 138, 125, 255),
+    32: (255, 220, 125, 255),
+    64: (255, 200, 125, 255),
+    128: (176, 100, 70, 255),
+    256: (191, 145, 71, 255),
+    512: (128, 53, 45, 255),
+    1024: (217, 178, 72, 255),
+    2048: (255, 198, 125, 255),
+    4096: (255, 156, 125, 255),
 }
 
 def move_board_up(board):
+    score = 0
     old_board = [row.copy() for row in board]
     for row in range(1, len(board)):
         if not any(board[row]): continue
@@ -40,22 +41,23 @@ def move_board_up(board):
                 board[row][col] = None
                 continue
             closest = None
-            for i, cell in list(enumerate(above))[::-1]:
-                if cell:
+            for i, tile in list(enumerate(above))[::-1]:
+                if tile:
                     closest = i
                     break
             if board[closest][col] == board[row][col]:
+                score += board[closest][col] * 2
                 board[closest][col] *= 2
                 board[row][col] = None
             elif not board[closest+1][col]:
                 board[closest+1][col] = board[row][col]
                 board[row][col] = None
-    return board, old_board != board
+    return board, old_board != board, score
 
 def move_board_down(board):
+    score = 0
     old_board = [row.copy() for row in board]
-    for row in range(len(board)-2, -1, -1):
-        if not any(board[row]): continue
+    for row in list(range(len(board)-1))[::-1]:
         for col in range(len(board)):
             if not board[row][col]: continue
             below = [r[col] for r in board[row+1:]]
@@ -64,19 +66,21 @@ def move_board_down(board):
                 board[row][col] = None
                 continue
             closest = None
-            for i, cell in list(enumerate(below))[::-1]:
-                if cell:
+            for i, tile in enumerate(below):
+                if tile:
                     closest = i
                     break
             if board[row+closest+1][col] == board[row][col]:
+                score += board[row+closest+1][col] * 2
                 board[row+closest+1][col] *= 2
                 board[row][col] = None
             elif not board[row+closest][col]:
                 board[row+closest][col] = board[row][col]
                 board[row][col] = None
-    return board, old_board != board
+    return board, old_board != board, score
 
 def move_board_left(board):
+    score = 0
     old_board = [row.copy() for row in board]
     for col in range(1, len(board)):
         if not any([r[col] for r in board]): continue
@@ -88,22 +92,23 @@ def move_board_left(board):
                 board[row][col] = None
                 continue
             closest = None
-            for i, cell in list(enumerate(left))[::-1]:
-                if cell:
+            for i, tile in list(enumerate(left))[::-1]:
+                if tile:
                     closest = i
                     break
             if board[row][closest] == board[row][col]:
+                score += board[row][closest] * 2
                 board[row][closest] *= 2
                 board[row][col] = None
             elif not board[row][closest+1]:
                 board[row][closest+1] = board[row][col]
                 board[row][col] = None
-    return board, old_board != board
+    return board, old_board != board, score
 
 def move_board_right(board):
+    score = 0
     old_board = [row.copy() for row in board]
-    for col in range(len(board)-2, -1, -1):
-        if not any([r[col] for r in board]): continue
+    for col in list(range(len(board)-1))[::-1]:
         for row in range(len(board)):
             if not board[row][col]: continue
             right = board[row][col+1:]
@@ -112,23 +117,24 @@ def move_board_right(board):
                 board[row][col] = None
                 continue
             closest = None
-            for i, cell in list(enumerate(right))[::-1]:
-                if cell:
+            for i, tile in enumerate(right):
+                if tile:
                     closest = i
                     break
-            if board[row][col+closest+1] == board[row][col]:
+            if board[row][col+1+closest] == board[row][col]:
+                score += board[row][col+closest+1] * 2
                 board[row][col+closest+1] *= 2
                 board[row][col] = None
             elif not board[row][col+closest]:
                 board[row][col+closest] = board[row][col]
                 board[row][col] = None
-    return board, old_board != board
+    return board, old_board != board, score
 
 def print_board(board, key=None):
     if key: print(key)
     for row in board:
-        for cell in row:
-            print(cell if cell else "_", end=" ")
+        for tile in row:
+            print(tile if tile else "_", end=" ")
         print()
     print()
 
@@ -149,8 +155,22 @@ def check_neighbours(board):
             if col < len(board)-1 and board[row][col+1] == current: return True
     return False
 
+def interpolate(start_value, end_value, step, total_steps):
+    delta = end_value - start_value
+    return start_value + (delta * (step / total_steps))
+
+def get_tiles_positions(board):
+    tiles = []
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if board[row][col]:
+                tiles.append = [board[row][col], (row * tile_size, col * tile_size)]
+    return tiles
+
 print_board(board)
 running = True
+score = 0
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -159,30 +179,31 @@ while running:
             pressed = event.key
             changed = False
             if pressed in [pygame.K_w, pygame.K_UP]:
-                board, changed = move_board_up(board)
+                board, changed, points = move_board_up(board)
             if pressed in [pygame.K_s, pygame.K_DOWN]:
-                board, changed = move_board_down(board)
+                board, changed, points = move_board_down(board)
             if pressed in [pygame.K_a, pygame.K_LEFT]:
-                board, changed = move_board_left(board)
+                board, changed, points = move_board_left(board)
             if pressed in [pygame.K_d, pygame.K_RIGHT]:
-                board, changed = move_board_right(board)
-            if changed: fill_board_random(board)
-            print_board(board, pressed)
+                board, changed, points = move_board_right(board)
+            if changed: fill_board_random(board); score += points
+            pygame.display.set_caption(f"2048 | Score: {score}")
+            # print_board(board, pressed)
 
     if not any([any(r) for r in board]):
         fill_board_random(board)
 
-    board_surf = pygame.Surface((board_size+4, board_size+4))
-    board_surf.fill((255, 255, 255))
+    board_surf = pygame.Surface((board_size+4, board_size+4), pygame.SRCALPHA)
+    board_surf.fill((255, 255, 255, 255))
     for row in range(len(board)):
         for col in range(len(board)):
             if board[row][col]:
-                rect_x = cell_size * col + 4
-                rect_y = cell_size * row + 4
-                rect_width = cell_size - 4
-                rect_height = cell_size - 4
+                rect_x = tile_size * col + 4
+                rect_y = tile_size * row + 4
+                rect_width = tile_size - 4
+                rect_height = tile_size - 4
                 pygame.draw.rect(board_surf, colors[board[row][col]], (rect_x, rect_y, rect_width, rect_height))
-                text_surf = font.render(str(board[row][col]), True, (255, 255, 255))
+                text_surf = font.render(str(board[row][col]), True, (255, 255, 255, 255))
                 text_x = rect_x + (rect_width - text_surf.get_width()) / 2
                 text_y = rect_y + (rect_height - text_surf.get_height()) / 2
                 board_surf.blit(text_surf, (text_x, text_y))
@@ -195,7 +216,13 @@ while running:
 
     if all([all(r) for r in board]) and not check_neighbours(board):
         print("Game Over")
-        time.sleep(2)
+        for i in range(10):
+            pygame.display.set_caption(f"2048 | Game Over | Score: {score}")
+            pygame.display.flip()
+            pygame.time.delay(200)
+            pygame.display.set_caption(f"2048 | Score: {score}")
+            pygame.display.flip()
+            pygame.time.delay(200)
         running = False
 
     clock.tick(60)
